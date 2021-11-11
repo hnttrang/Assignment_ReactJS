cf022feff77b19b85d55ddc2c61c2ac55d405f47
hongocthaotrang <trangho.2632@gmail.com>
2021-11-11 09:02:04 +0700

    Reconstruct UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/css/style.css';
+import Logo from './assets/logo.svg';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+import Display from './components/Display/Display';
+
+//const tippad
+let data = {
+  amount : '0.00', total : '0.00'
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //  />
+    <body>
+      <div className="logo"><img src={Logo} /></div>
+      <div className="main">
+      <Input>
+        <Button
+        className="percent-button"
+        value = "0"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("clicked");
+        }}>
+        </Button>
+      </Input>
+
+      <Display
+      total = {data.total}
+      amount = {data.amount}
+      >
+
+      </Display>
+      </div>
+    </body>
   );
 }
 
